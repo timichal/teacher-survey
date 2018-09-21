2ebcc1d1a523a4850a3c7143da0de4ee8af66605
timichal <michal@zlatkovsky.cz>
2018-09-21 10:12:14 +0200

    part 1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 //require('bootstrap');
+import $ from "jquery";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css'
-let test = () => console.log("test!")
 
-test()
+$(function () {
 
-window.onbeforeunload = function() {
+    $('.consent_btn').click(function() {
+        if ($('#consent1_yes').is(':checked') && $('#consent2_yes').is(':checked') && $('#consent3_yes').is(':checked') && $('#consent4_yes').is(':checked')) {
+            $('#toPart2').prop('disabled', false);
+        } else {
+            $('#toPart2').prop('disabled', true);
+        }
+    });
+
+    $("#toPart2").click(function() {
+        $("#content_1").hide(500);
+        $("#content_2").show("fast");
+    });
+});
+
+/*window.onbeforeunload = function() {
     return 'Are you sure you want to navigate away from this page?';
-};
+};*/
