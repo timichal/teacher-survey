bf31117d580f2d05c6fcf9879aeb5b40e4ea7176
timichal <michal@zlatkovsky.cz>
2018-09-21 17:19:02 +0200

    prototyp

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './styles.css'
 $(function () {
     const stamp = Date.now() + Math.floor(Math.random() * 10000);
     let answers = {};
+    answers.uid = stamp;
 
     $('.consent_btn').click(function() {
         if ($('#consent1_yes').is(':checked') && $('#consent2_yes').is(':checked') && $('#consent3_yes').is(':checked') && $('#consent4_yes').is(':checked')) {
@@ -20,34 +21,66 @@ $(function () {
         $(".visible").hide(500).removeClass("visible");
         let nextPart = $(this).attr("data-to-part");
         $("#content_"+nextPart).show("fast").addClass("visible");
+        $("html, body").animate({ scrollTop: 200 }, "fast");
+        // window unload alert, removed at the end
+        window.onbeforeunload = function() {
+            return 'Opravdu chcete opustit tuto stránku? Můžete přijít o vyplněné odpovědi!';
+        };
+
+        if (nextPart === "end") {
+            window.onbeforeunload = function() {
+                return undefined;
+            }
+        }
 
         // we use the page switching buttons to save the answers as well
-        let radio_questions = ["q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9"]
+        let radio_questions = ["q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11", "q12", "q13", "q14", "q15", "q16", "q17", "q18"]
         radio_questions.forEach((question) => {
             answers[question] = $("[name='" + question + "']:checked").val();
         })
 
-        let text_questions = ["part1_feedback", "part2_feedback", "part3_feedback"]
+        let text_questions = ["part1_feedback", "part2_feedback", "part3_feedback", "popularity", "email_input"]
         text_questions.forEach((question) => {
             answers[question] = $("[name='" + question + "'").val();
         })
-        
+
+        // answers for the reflection checkboxes & freeform form an array
+        let reflection_answers = [];
+        $("[name='reflection']:checked").each(function() {
+            reflection_answers.push($(this).val());
+        })
+        if ($("[name='reflection_free']").val()) {
+            reflection_answers.push($("[name='reflection_free']").val());
+        }
+
+        if (reflection_answers.length > 0) {
+            answers.reflection = reflection_answers;
+        }
+
+        // sending the answers when the user reaches the end
+        if (nextPart === "end") {
+            sendAnswers(answers);
+        }
+
         console.log(answers);
     });
 });
 
-/*
-$.ajax({
-    url: "https://ah3uqnxmak.execute-api.eu-central-1.amazonaws.com/prod",
-    type: "POST",
-    crossDomain: !0,
-    contentType: "application/json",
-    data: JSON.stringify({ "user_id": stamp, "q_id": qid, "val": val }),
-    dataType: "json",
-    success: function success(response) {
-*/
-/*
-window.onbeforeunload = function() {
-    console.log("quitting!");
-    return 'Opravdu chcete opustit tuto stránku? Můžete přijít o vyplněné odpovědi!';
-};*/
+function sendAnswers(answers) {
+    for (var answer in answers) {
+        if (!answers[answer])  {
+            delete answers[answer]
+        }
+    }
+
+    $.ajax({
+        url: "https://fo6tnkl1i8.execute-api.eu-central-1.amazonaws.com/prod",
+        type: "POST",
+        crossDomain: !0,
+        contentType: "application/json",
+        data: JSON.stringify(answers),
+        dataType: "json",
+        success: function success(response) {
+        }
+    });
+}
