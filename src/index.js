13e492bd856ce5af8069d66a8ca5f07c32d049f3
timichal <michal@zlatkovsky.cz>
2018-09-21 09:01:47 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,6 @@
+//require('bootstrap');
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css'
+let test = () => console.log("test!")
+
+test()
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  node: {
+    fs: 'empty'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'/*,'@babel/preset-react'*/]
+          }
+        }
+      }, {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+};
